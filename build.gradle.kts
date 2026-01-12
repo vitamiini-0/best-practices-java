a8fd5f7af7d353f159c1c47d34ab4c938e2a6d2d
vitamiini-0 <228652369+vitamiini-0@users.noreply.github.com>
2026-01-12 15:34:32 +0200

    add pre-commit config

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     id("java")
+
+    id("com.diffplug.spotless") version "6.25.0"
 }
 
 group = "org.example"
@@ -13,6 +15,15 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+spotless {
+    java {
+        googleJavaFormat()
+        removeUnusedImports()
+        trimTrailingWhitespace()
+        endWithNewline()
+    }
 }
 
 tasks.test {
