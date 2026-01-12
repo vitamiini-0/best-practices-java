b3638b94d73c22df902659d3f42f06beb9215808
vitamiini-0 <228652369+vitamiini-0@users.noreply.github.com>
2026-01-12 15:39:43 +0200

    fix: apply spotless fixes

--- src/test/java/org/example/TestMain.java
+++ src/test/java/org/example/TestMain.java
@@ -1,12 +1,12 @@
 package org.example;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.Test;
+
 public class TestMain {
-    @Test
-    public void testAdd() {
-        assertEquals(4, Main.add(1, 3));
-    }
+  @Test
+  public void testAdd() {
+    assertEquals(4, Main.add(1, 3));
+  }
 }
